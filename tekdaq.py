52a70fbd1355c249947b65be35d6b822ae56e97d
Sam de Jong <samdejong86@gmail.com>
2018-02-06 14:14:21 -0800

    bugfixes, added comments

--- tekdaq.py
+++ tekdaq.py
@@ -13,13 +13,11 @@ import ROOT as r
 from ROOT import TFile, TTree, AddressOf, gROOT
 import numpy as np
 
-
+#parse command line arguments
 parser = argparse.ArgumentParser("Read data from a Tektronix TDS 3052 oscilloscpe via an RS-232 port")
 parser.add_argument('-p','--port', help='The port to listen to', default="/dev/ttyUSB0", required=False)
 parser.add_argument('-r','--baudrate', help='baud rate of port', default=38400, required=False)
-
 parser.add_argument('-u','--unlock', help='Unlock front panel then exit', action='store_true', required=False)
-
 parser.add_argument('-o','--output', help='Name of data file', default="tek.root", required=False, metavar='FILE')
 parser.add_argument('-k','--keep', help='Keep existing scope settings, ignoring other command line arguments.', action='store_true', required=False)
 parser.add_argument('-w','--wave', help='Record waveform data for channel CH; specify \'a\' for all channels.', default='a', required=False, metavar='CH', choices=['a','1','2'])
@@ -28,23 +26,24 @@ parser.add_argument('-n','--nevents', help='Number of events to record', default
 parser.add_argument('-c', '--trsrc', help='Specify the trigger channel; specify \'0\' for \'EXT\'', default='1', required=False, metavar='CH', choices=['0','1','2'])
 parser.add_argument('-t','--trlevel', help='Specify trigger level (in volts).', default='1E0', required=False, metavar='TRIG_LEVEL')
 parser.add_argument('-s', '--trslope', help='Specify the trigger edge slope - FALL or RISE.', default='RISE', required=False, metavar='TRIG_SLOPE', choices=['RISE','FALL'])
-
 parser.add_argument('--vsca1', help='Specify vertical scale (in volts) for channel 1.', default= '200E-3', required=False, metavar='VSCALE')
 parser.add_argument('--vsca2', help='Specify vertical scale (in volts) for channel 2.', default= '200E-3', required=False, metavar='VSCALE')
 parser.add_argument('--coupl1', help='Specify coupling for channel 1, \'AC\' or \'DC\'; default is \'DC\'.',  default='DC', required=False, metavar='COUPL', choices=['AC', 'DC'])
 parser.add_argument('--coupl2', help='Specify coupling for channel 2.',  default='DC', required=False, metavar='COUPL', choices=['AC', 'DC'])
 parser.add_argument('--imped1', help='Specify impedance for channel 1, \'FIF\' or \'MEG\'; default is \'MEG\'.',  default='MEG', required=False, metavar='IMPED', choices=['FIF', 'MEG'])
 parser.add_argument('--imped2', help='Specify impedance for channel 2.',  default='MEG', required=False, metavar='IMPED', choices=['FIF', 'MEG'])
-
 parser.add_argument('-b','--hsamp', help='Specify the horizontal scale (in seconds); note that this can effect the sample rate.', default='20.E-9', required=False)
 parser.add_argument('-pt','--pretrigger', help='Specify the amount of pretrigger (percent).', default='20', required=False)
 
 
 args = parser.parse_args()
 
+#open the serial port
 port = Serial(args.port, args.baudrate, timeout=1)
+#connect to pytek
 tds = TDS3k(port)
 
+#unlock the scope if requested
 if args.unlock:
     tds.send_command("LOC NONE")
     exit()
@@ -52,16 +51,16 @@ if args.unlock:
 
 
 # Make the scope identify itself.
+print(tds.identify())
 
 
 vsca1=args.vsca1
 vsca2=args.vsca2
 hsamp=args.hsamp
 
-print(tds.identify())
-
 getdata = [False, False]
 
+#apply settings if '-k' setting not used
 if not args.keep:
     if args.wave == 'a' or args.wave =='1':
         tds.send_command("CH1:SCA "+args.vsca1)
@@ -86,7 +85,8 @@ if not args.keep:
     tds.send_command("HOR:SCA "+args.hsamp)
     tds.send_command("HOR:TRIG:POS "+args.pretrigger)
 
-else:
+    
+else: #if '-k' used, get the horizontal and vertical scale
     temp=tds.send_query("HORIZONTAL")
     hsamp=temp.split(';')[2]
     
@@ -95,7 +95,7 @@ else:
 
 tds.send_command("HOR:RECORDLENGTH "+args.length)    
 
-
+#set the waveform flags
 if args.wave == 'a' or args.wave =='1':
     getdata[0]=True
 if args.wave == 'a' or args.wave =='2':
@@ -103,7 +103,7 @@ if args.wave == 'a' or args.wave =='2':
     
 
 Preambles = []
-
+#get the waveform preablmes
 for ch in range(2):
     if getdata[ch]:
         c1=tds.get_curve("CH"+str(ch+1))
@@ -112,15 +112,37 @@ for ch in range(2):
         Preambles.append(0)
 
 
+#lock the 'scope, set to single seq mode
 tds.send_command("LOC All")
 tds.send_command("ACQ:STOPA SEQ")
 
+#open root file
+f=TFile(args.output, 'recreate')
+t=TTree("data", "data")
+
+#create vectors
+vectors=[]
+vectors.append(r.vector('double')())
+vectors.append(r.vector('double')())
+
+#assign branches
+for ch in range(2):
+    if getdata[ch]:
+        t.Branch('ch'+str(ch+1), vectors[ch])
+
+#assign xincrement branch
+xinc = np.zeros(1, dtype=float)
+t.Branch('xinc', xinc, 'xinc/D')
+xinc[0]=float(Preambles[0]['x_incr'])
 
 
+
+
+#horizontal max and min of graph
 xmin=-5*float(hsamp)
 xmax=5*float(hsamp)
 
-
+#vertical max and min of graph
 ybase=0.0
 if args.wave == '1':
     ybase=5.*float(vsca1)
@@ -129,29 +151,8 @@ elif args.wave == '2':
 elif args.wave == 'a':
     ybase=max(5.*float(vsca2),5.*float(vsca1))
 
-
-    
-
 ymin=-1.*ybase
 ymax=ybase
-
-f=TFile(args.output, 'recreate')
-t=TTree("data", "data")
-
-vectors=[]
-vectors.append(r.vector('double')())
-vectors.append(r.vector('double')())
-
-for ch in range(2):
-    if getdata[ch]:
-        t.Branch('ch'+str(ch+1), vectors[ch])
-
-        
-xinc = np.zeros(1, dtype=float)
-t.Branch('xinc', xinc, 'xinc/D')
-xinc[0]=float(Preambles[0]['x_incr'])
-
-        
 
     
     
@@ -161,9 +162,8 @@ ax = plt.axes(xlim=(xmin, xmax), ylim=(ymin, ymax))
 lines = []
 lobj = ax.plot([], [], 'r-', animated=True)[0]
 ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
-#ax.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
 
-
+#set colours of each waveform
 plotlays, plotcols, plotstyle, linw = [2], ["#DCBF73","#6E95B4"], ['', ''], [2,2]
 
 for index in range(2):
@@ -178,28 +178,43 @@ def init():
         lines[i].set_data([],[])
     return lines
 
+# close the scope and file
+def finished():
+    f.Write()
+    f.Close()
+    
+    tds.send_command("LOC NONE")
+    
+    tds.close()
+    exit()
+
 
 numEvents=0
 
 # animation function.  This is called sequentially
 def animate(i):
-
     global numEvents
+
+
+    # exit on end of run
+    if numEvents >= int(args.nevents):
+        finished()
+    
     numEvents = numEvents+1
 
     global t
     global vectors
+    global Preambles
 
-    if i > numEvents:
-        return lines
-
-    global Preambles;
-
+    #set acquire state
     tds.send_command("ACQ:STATE ON")
-    
+
+    #get curves
     for ch in range(2):
         if getdata[ch]:
+            #get curve
             curve = tds.get_curve("CH"+str(ch+1))
+            #use waveform header to convert ADC counts to volts
             data = (
                 (float(Preambles[ch]["xzero"]) + i*float(Preambles[ch]["x_incr"]), ((curve[i] - float(Preambles[ch]["y_offset"])) * float(Preambles[ch]["y_scale"])) + float(Preambles[ch]["y_zero"]))
                 for i in range(len(curve))
@@ -209,6 +224,7 @@ def animate(i):
             xdat=[]
             ydat=[]
 
+            #add data to graph and root file
             vectors[ch].clear()
             for x,y in data:
                 xdat.append(x)
@@ -217,11 +233,12 @@ def animate(i):
 
             lines[ch].set_data(xdat,ydat)     
 
-            t.Fill()
             
         else:
             lines[ch].set_data(0,0)
-                
+
+    #fill ttree
+    t.Fill()    
     return lines
 
 
@@ -237,10 +254,6 @@ plt.ylabel(tds.y_units())
 plt.xlabel(tds.x_units())
 plt.show()
 
-f.Write()
-f.Close()
 
 
-tds.send_command("LOC NONE")
-
-tds.close()
+finished()
